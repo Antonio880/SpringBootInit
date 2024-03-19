b31b247fbcef7ffd460eecd4214038dc49022dba
Antonio880 <antoniocruzgomes880@gmail.com>
2024-03-19 10:37:28 -0300

    Colocando Aplicação Spring de API de Séries, ainda no terminal

---
+++ src/main/java/br/com/antonio/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,21 @@
+package br.com.antonio.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.antonio.screenmatch.principal.Principal;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
+}
